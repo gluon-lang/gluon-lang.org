e40cbce4267ef0c9800bda634a81ab01c3aaf2bf
Markus Westerlind <marwes91@gmail.com>
2016-09-17 13:13:08 +0200

    feat: Limit the amount of memory or cpu time an eval request may take

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ extern crate staticfile;
 extern crate mount;
 extern crate gluon;
 extern crate serde_json;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 use std::io::Read;
+use std::time::Instant;
 
 use iron::prelude::*;
 use iron::status;
@@ -16,23 +20,44 @@ use staticfile::Static;
 
 use mount::Mount;
 
-use gluon::vm::thread::RootedThread;
-use gluon::vm::api::generic::A;
+use gluon::vm::thread::{RootedThread, ThreadInternal};
+use gluon::vm::Error;
 use gluon::vm::api::Generic;
+use gluon::vm::api::generic::A;
 use gluon::{Compiler, new_vm};
 
 pub struct VMKey;
-impl Key for VMKey { type Value = RootedThread; }
+impl Key for VMKey {
+    type Value = RootedThread;
+}
 
 fn eval(req: &mut Request) -> IronResult<Response> {
-    eval_(req)
-        .map(|s| Response::with((status::Ok, ::serde_json::to_string(&s).unwrap())))
+    eval_(req).map(|s| Response::with((status::Ok, ::serde_json::to_string(&s).unwrap())))
 }
 fn eval_(req: &mut Request) -> IronResult<String> {
     let mut body = String::new();
     itry!(req.body.read_to_string(&mut body));
+    info!("Eval: `{}`", body);
+    body.push(' ');
     let global_vm = req.get::<persistent::Read<VMKey>>().unwrap();
-    let vm = global_vm.new_thread().expect("New thread");
+    let vm = match global_vm.new_thread() {
+        Ok(vm) => vm,
+        Err(err) => return Ok(format!("{}", err)),
+    };
+
+    // Prevent a single thread from allocating to much memory
+    vm.set_memory_limit(2_000_000);
+
+    // Prevent infinite loops from running forever
+    let start = Instant::now();
+    vm.context().set_hook(Some(Box::new(move |_| {
+        if start.elapsed().as_secs() < 10 {
+            Ok(())
+        } else {
+            Err(Error::Message("Thread has exceeded the allowed exection time".into()))
+        }
+    })));
+
     let (value, typ) = match Compiler::new().run_expr::<Generic<A>>(&vm, "<top>", &body) {
         Ok(value) => value,
         Err(err) => return Ok(format!("{}", err)),
@@ -41,6 +66,7 @@ fn eval_(req: &mut Request) -> IronResult<String> {
 }
 
 fn main() {
+    env_logger::init().unwrap();
     let mut mount = Mount::new();
 
     mount.mount("/", Static::new("public"));
@@ -50,5 +76,7 @@ fn main() {
     middleware.link(persistent::Read::<VMKey>::both(vm));
     mount.mount("/eval", middleware);
 
-    Iron::new(mount).http("0.0.0.0:8080").unwrap();
+    let address = "0.0.0.0:8080";
+    let _server = Iron::new(mount).http(address).unwrap();
+    println!("Server started at `{}`", address);
 }
