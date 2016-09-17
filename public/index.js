e7ac1f9389692917ccb37041ef4834b495373d48
Markus Westerlind <marwes91@gmail.com>
2016-09-17 13:13:08 +0200

    feat: Add a list of examples to the site

--- public/index.js
+++ public/index.js
@@ -20,18 +20,60 @@ var Eval = React.createClass({
           }.bind(this)
         });
     },
+    updateExample: function(example) {
+        this.setState({text: example });
+    },
     render: function() {
         return (
             <div>
+                <Examples url="examples" updateExample={this.updateExample} />
                 <form url="eval" onSubmit={this.handleSubmit}>
                     <textarea value={this.state.text} onChange={this.handleTextChange} name="expr" cols="100" rows="30">
                     </textarea>
+                    <br/>
                     <input type="submit" value="Eval"/>
                 </form>
                 <h3>Result</h3>
                 <pre>{this.state.result}</pre>
             </div>
         );
+    }
+});
+
+var Examples = React.createClass({
+    propTypes: {
+        url: React.PropTypes.string.isRequired,
+        updateExample: React.PropTypes.func.isRequired,
+    },
+    getInitialState: function() {
+        return {
+            options: [],
+        }
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: this.successHandler
+        })
+    },
+    successHandler: function(data) {
+        for (var i = 0; i < data.length; i++) {
+            var option = data[i];
+            this.state.options.push(
+                <option key={i} value={option.value}>{option.name}</option>
+            );
+        }
+        // Initialize textarea with the first example
+        this.props.updateExample(data[0].value);
+        this.forceUpdate();
+    },
+    changeHandler: function(e) {
+        console.log("ads")
+        this.props.updateExample(e.target.value);
+    },
+    render: function() {
+        return <select onChange={this.changeHandler}>{this.state.options}</select>;
     }
 });
 
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,15 @@ extern crate serde_json;
 extern crate log;
 extern crate env_logger;
 
-use std::io::Read;
+use std::fs::{File, read_dir};
+use std::io::{self, Read};
 use std::time::Instant;
 
 use iron::prelude::*;
 use iron::status;
 use iron::typemap::Key;
+
+use serde_json::Value;
 
 use staticfile::Static;
 
@@ -66,32 +69,70 @@ fn eval_(req: &mut Request) -> IronResult<String> {
     Ok(format!("{:?} : {}", value, typ))
 }
 
+pub struct Examples;
+
+impl Key for Examples {
+    type Value = String;
+}
+
+fn examples(req: &mut Request) -> IronResult<Response> {
+    let s = req.get::<persistent::Read<Examples>>().unwrap();
+    Ok(Response::with((status::Ok, (*s).clone())))
+}
+
+fn load_examples() -> Value {
+    let vec = read_dir("public/examples")
+        .unwrap()
+        .map(|entry| {
+            let path = try!(entry).path();
+            let name = String::from(path.file_stem().unwrap().to_str().unwrap());
+            let mut file = try!(File::open(path));
+            let mut contents = String::new();
+            try!(file.read_to_string(&mut contents));
+            Ok(Value::Object(vec![("name".into(), Value::String(name)),
+                                  ("value".into(), Value::String(contents))]
+                .into_iter()
+                .collect()))
+        })
+        .collect::<io::Result<_>>()
+        .unwrap();
+    Value::Array(vec)
+}
+
 fn main() {
     env_logger::init().unwrap();
     let mut mount = Mount::new();
 
     mount.mount("/", Static::new("public"));
 
-    let mut middleware = Chain::new(eval);
-    let vm = RootedThread::new();
-
-    // Ensure the import macro cannot be abused to to open files
     {
-        // Ensure the lock to `paths` are released
-        let import = Import::new(DefaultImporter);
-        import.paths.write().unwrap().clear();
-        vm.get_macros()
-            .insert(String::from("import"), import);
-    }
-    Compiler::new()
-        .implicit_prelude(false)
-        .run_expr::<OpaqueValue<&Thread, Hole>>(&vm, "", r#" import "std/types.glu" "#)
-        .unwrap();
-    gluon::vm::primitives::load(&vm).expect("Loaded primitives library");
-    gluon::io::load(&vm).expect("Loaded IO library");
+        let mut middleware = Chain::new(eval);
+        let vm = RootedThread::new();
 
-    middleware.link(persistent::Read::<VMKey>::both(vm));
-    mount.mount("/eval", middleware);
+        // Ensure the import macro cannot be abused to to open files
+        {
+            // Ensure the lock to `paths` are released
+            let import = Import::new(DefaultImporter);
+            import.paths.write().unwrap().clear();
+            vm.get_macros()
+                .insert(String::from("import"), import);
+        }
+        Compiler::new()
+            .implicit_prelude(false)
+            .run_expr::<OpaqueValue<&Thread, Hole>>(&vm, "", r#" import "std/types.glu" "#)
+            .unwrap();
+        gluon::vm::primitives::load(&vm).expect("Loaded primitives library");
+        gluon::io::load(&vm).expect("Loaded IO library");
+
+        middleware.link(persistent::Read::<VMKey>::both(vm));
+        mount.mount("/eval", middleware);
+    }
+    {
+        let mut middleware = Chain::new(examples);
+        let examples_string = serde_json::to_string(&load_examples()).unwrap();
+        middleware.link(persistent::Read::<Examples>::both(examples_string));
+        mount.mount("/examples", middleware);
+    }
 
     let address = "0.0.0.0:8080";
     let _server = Iron::new(mount).http(address).unwrap();
