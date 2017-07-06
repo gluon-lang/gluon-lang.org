57e9c0f9d3f26f2428d2c9e9e6ae43fd69c14043
Markus Westerlind <marwes91@gmail.com>
2017-07-06 21:28:16 +0200

    Kill the old server before starting the new one

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cargo build --release && RUST_LOG=info cargo run --release &> output
+
+cargo build --release
+
+OLD_GROUP_ID=$(ps x -o  "%p %r %y %x %c " | grep cargo | awk  '{print $2}')
+kill -- -$OLD_GROUP_ID
+RUST_LOG=info cargo run --release &> output
