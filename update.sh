9625df1517805bb6113a914ae99f7d152bbcef5c
Markus Westerlind <marwes91@gmail.com>
2017-07-06 20:37:30 +0200

    Tweak the update.sh script

--- update.sh
+++ update.sh
@@ -1,1 +1,2 @@
-cd $HOME/try_gluon && git pull origin master --ff-only && RUSTST_LOG=info cargo run --release &> output
+#!/bin/bash
+cd $HOME/try_gluon && git pull origin master --ff-only && RUST_LOG=info cargo run --release &> output
