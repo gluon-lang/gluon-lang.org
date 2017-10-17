014e1dba174cd48f76bdea635201b1e7e7f39e5d
EC2 Default User <ec2-user@ip-172-31-26-137.eu-central-1.compute.internal>
2017-10-17 17:34:34 +0000

    Add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+SWAP=/mnt/1GB.swap
+
+fallocate -l 1G ${SWAP}
+mkswap ${SWAP}
+swapon ${SWAP}
+
+echo "${SWAP}  none  swap  sw 0  0"
+
+swapon -s
+
+iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8 080
+sh -c "iptables-save > /etc/iptables.rules"
