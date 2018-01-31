2bbdd93cdb563c1b528f253c5c8785f6b8001642
Markus Westerlind <marwes91@gmail.com>
2018-01-31 22:56:32 +0100

    Name the server try_gluon

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 webpack
 cargo build --release
 
-OLD_GROUP_ID=$(ps x -o  "%p %r %y %x %c " | grep cargo | awk  '{print $2}')
+OLD_GROUP_ID=$(ps x -o  "%p %r %y %x %c " | grep try_gluon | awk  '{print $2}')
 if [ -n $OLD_GROUP_ID ]; then
 	kill -- -$OLD_GROUP_ID
 fi
