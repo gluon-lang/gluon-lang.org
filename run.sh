719003ceb4b6e580d42850da882fadda654f6b93
Markus Westerlind <marwes91@gmail.com>
2018-02-03 14:52:00 +0100

    fix: Avoid locking the build directory while running the server

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ OLD_GROUP_ID=$(ps x -o  "%p %r %y %x %c " | grep try_gluon | awk  '{print $2}')
 if [ -n $OLD_GROUP_ID ]; then
 	kill -- -$OLD_GROUP_ID
 fi
-RUST_LOG=info cargo run --release &> output
+RUST_LOG=info target/release/try_gluon --release &> output
