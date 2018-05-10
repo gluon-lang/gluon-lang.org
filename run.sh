394d5af8e1c71b827f049324d05875c206468f72
Markus Westerlind <marwes91@gmail.com>
2018-05-10 13:57:07 +0200

    Avoid using || true in the run.sh script

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-set -x
+set -ex
 
 docker build --tag try_gluon .
 
-docker stop try_gluon_running || true
-docker rm try_gluon_running || true
+docker rm --force try_gluon_running
 
 RUST_LOG=info docker run -p 80:8080 --name try_gluon_running --env RUST_LOG try_gluon
