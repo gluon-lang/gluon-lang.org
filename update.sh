c7375dd191d93f999c0571fb1318de19f110da6a
Markus Westerlind <marwes91@gmail.com>
2018-05-10 12:55:24 +0200

    Reset the lock file before updating

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@ set -ex
 
 cd $HOME/try_gluon
 
+# The lock file may have been updated so reset it before pulling new changes
+git checkout Cargo.lock
 git pull origin master --ff-only
 cargo update -p https://github.com/gluon-lang/gluon
 
