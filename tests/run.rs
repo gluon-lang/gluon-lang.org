0c34e5c9d0f98195bcc84966d8e6bd3302331f1c
Markus Westerlind <marwes91@gmail.com>
2018-05-10 12:42:59 +0200

    Fix the test

--- tests/run.rs
+++ tests/run.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
@@ -7,18 +7,16 @@ fn test() {
     let path = PathBuf::from(::std::env::args().next().unwrap());
     let exe = path.parent()
         .and_then(|path| path.parent())
-        .expect("debugger executable")
+        .expect("server executable")
         .join(env!("CARGO_PKG_NAME"));
     let mut child = Command::new(exe).stdout(Stdio::piped()).spawn().unwrap();
     {
         let stdout = child.stdout.as_mut().expect("stdout missing");
-        let mut buffer = [0; 256];
-        let n = stdout.read(&mut buffer).unwrap();
-        if n == 0 {
-            panic!("Unable to read anything")
-        }
-        if !buffer.starts_with(b"Server started") {
-            panic!("Expected the server to start");
+        if let None = BufReader::new(stdout)
+            .lines()
+            .find(|line| line.as_ref().unwrap().starts_with("Server started"))
+        {
+            panic!("Expected the server to start",);
         }
     }
     child.kill().unwrap();
