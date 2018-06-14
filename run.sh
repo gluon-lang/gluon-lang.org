3d83750db4fb4fb48735ed822823f7e1a7aa7266
Markus Westerlind <marwes91@gmail.com>
2018-06-14 23:11:56 +0200

    Remove the docker container after it exits

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 set -ex
 
-docker system prune
 docker build --tag try_gluon .
 
 docker rm --force try_gluon_running
 
 RUST_LOG=info docker run \
+    -rm
     -p 80:8080 \
     --name try_gluon_running \
     --env RUST_LOG try_gluon \
