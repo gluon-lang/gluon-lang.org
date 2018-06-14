bc779b05cc325027c42edd291c2582ccd2f35953
Markus Westerlind <marwes91@gmail.com>
2018-06-14 23:57:45 +0200

    Fix the --rm flag

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker build --tag try_gluon .
 docker rm --force try_gluon_running
 
 RUST_LOG=info docker run \
-    -rm
+    --rm \
     -p 80:8080 \
     --name try_gluon_running \
     --env RUST_LOG try_gluon \
