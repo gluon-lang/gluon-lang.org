ad5156d8c0589c079cb885a9382bf152b4760263
Markus Westerlind <marwes91@gmail.com>
2018-06-14 23:10:21 +0200

    Try to use volumes to cache the rust build intermediates

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 set -ex
 
+docker system prune
 docker build --tag try_gluon .
 
 docker rm --force try_gluon_running
 
-RUST_LOG=info docker run -p 80:8080 --name try_gluon_running --env RUST_LOG try_gluon
+RUST_LOG=info docker run \
+    -p 80:8080 \
+    --name try_gluon_running \
+    --env RUST_LOG try_gluon \
+    --volume cargo:/usr/local/cargo \
+    --volume target:/usr/src/try_gluon/target
