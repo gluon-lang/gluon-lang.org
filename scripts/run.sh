d9c6cb227fc8935e8d9a9ec8e9471ec6d137ad43
Markus Westerlind <marwes91@gmail.com>
2018-10-10 22:50:33 +0200

    Use port 80 always

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ docker rm --force try_gluon_running || true
 
 RUST_LOG=info docker run \
     --rm \
-    -p 80:8080 \
+    -p 80:80 \
     --name try_gluon_running \
     --env RUST_LOG \
     --env-file try_gluon.env \
