32a22edae7893bca274b7ad84eacfd456761b31d
Markus Westerlind <marwes91@gmail.com>
2018-11-01 08:10:22 +0100

    Don't deleted the container/logs on shutdown

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,6 @@ docker pull marwes/try_gluon
 docker rm --force try_gluon_running || true
 
 RUST_LOG=try_gluon=info,warn docker run \
-    --rm \
     -p 80:80 \
     -p 443:443 \
     --name try_gluon_running \
