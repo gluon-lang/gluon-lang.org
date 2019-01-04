03bbe788a86ca788b98f21696108021f31f815b1
Markus Westerlind <marwes91@gmail.com>
2019-01-04 21:07:00 +0100

    feat: Add a 404 page

--- tests/run.rs
+++ tests/run.rs
@@ -1,7 +1,7 @@
 use hyper;
 
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::Command;
 
 use futures::{future, Future};
 use tokio::runtime::current_thread::Runtime;
@@ -9,30 +9,36 @@ use tokio_retry::{strategy::FixedInterval, Retry};
 
 #[test]
 fn test() {
+    test_pages(&[
+        ("", hyper::StatusCode::OK),
+        ("/not_existing.html", hyper::StatusCode::NOT_FOUND),
+        ("/404.html", hyper::StatusCode::OK),
+    ]);
+}
+
+fn test_pages(pages: &[(&str, hyper::StatusCode)]) {
     let path = PathBuf::from(::std::env::args().next().unwrap());
     let exe = path
         .parent()
         .and_then(|path| path.parent())
         .expect("server executable")
         .join(env!("CARGO_PKG_NAME"));
-    let mut child = Command::new(exe)
-        .args(&["--port", "4567"])
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+    let mut child = Command::new(exe).args(&["--port", "4567"]).spawn().unwrap();
     {
         let mut runtime = Runtime::new().unwrap();
 
-        runtime
-            .block_on(future::lazy(move || {
-                let client = hyper::Client::new();
-                let strategy = FixedInterval::from_millis(500).take(20);
-                Retry::spawn(strategy, move || {
-                    client.get("http://localhost:4567".parse().unwrap())
-                })
-                .map(|response| assert_eq!(response.status(), hyper::StatusCode::OK))
-            }))
-            .unwrap();
+        for (page, expected_status) in pages {
+            runtime
+                .block_on(future::lazy(move || {
+                    let client = hyper::Client::new();
+                    let strategy = FixedInterval::from_millis(500).take(20);
+                    Retry::spawn(strategy, move || {
+                        client.get(format!("http://localhost:4567{}", page).parse().unwrap())
+                    })
+                    .map(move |response| assert_eq!(response.status(), *expected_status))
+                }))
+                .unwrap();
+        }
     }
     child.kill().unwrap();
 }
