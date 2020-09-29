7a156c79b278f9f81740fbcf742dc688b77af8f4
Markus Westerlind <marwes91@gmail.com>
2020-09-29 20:30:02 +0200

    ec2 terraform

--- terraform/task.tf
+++ terraform/task.tf
@@ -9,8 +9,6 @@ locals {
   {
     "image": "marwes/try_gluon",
     "name": "gluon-lang",
-    "cpu": 0,
-    "memory": 128,
     "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
@@ -18,7 +16,15 @@ locals {
         "awslogs-group" : "${aws_cloudwatch_log_group.gluon-lang.name}",
         "awslogs-stream-prefix" : "gluon-lang"
       }
-    }
+    },
+    "portMappings": [
+        {
+            "containerPort": 80
+        },
+        {
+            "containerPort": 443
+        }
+    ]
   }
 ]
 DEFINITION
@@ -29,10 +35,9 @@ resource "aws_ecs_task_definition" "gluon_lang" {
   family                   = "gluon-lang"
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  cpu                      = "256"
-  memory                   = "1024"
+  memory                   = "256"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  requires_compatibilities = ["EC2"]
   container_definitions = local.container_definitions
 }
 
@@ -40,7 +45,7 @@ resource "aws_ecs_service" "gluon-lang" {
   name            = "gluon-lang"
   cluster         = aws_ecs_cluster.gluon-lang.id
   task_definition = aws_ecs_task_definition.gluon_lang.arn
-  launch_type     = "FARGATE"
+  launch_type     = "EC2"
 
   desired_count = 1
 
@@ -49,13 +54,12 @@ resource "aws_ecs_service" "gluon-lang" {
 
   network_configuration {
      subnets = aws_subnet.gluon-lang.*.id
-     assign_public_ip = "true"
 
-     security_groups = [aws_security_group.gluon_lang.id]
+     security_groups = [aws_security_group.gluon-lang.id]
   }
 }
 
-resource "aws_security_group" "gluon_lang" {
+resource "aws_security_group" "gluon-lang" {
   name_prefix = "gluon-lang-"
   vpc_id = aws_vpc.aws-vpc.id
 
@@ -76,4 +80,53 @@ resource "aws_security_group" "gluon_lang" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+
+data "aws_ami" "ecs_optimized" {
+  most_recent = true
+  owners = ["591542846629"] # AWS
+
+  filter {
+      name   = "name"
+      values = ["*amazon-ecs-optimized"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+resource "aws_launch_configuration" "launch" {
+  name          = "web_config"
+  image_id      = data.aws_ami.ecs_optimized.id
+  security_groups = [aws_security_group.gluon-lang.id]
+  instance_type = "t2.micro"
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user_data.yaml")}"
+
+  vars = {
+    ecs_cluster = "gluon-lang"
+  }
+}
+
+resource "aws_instance" "gluon-lang" {
+  ami                    = data.aws_ami.ecs_optimized.id
+  subnet_id              = aws_subnet.gluon-lang.id
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.gluon-lang.id]
+  ebs_optimized          = "false"
+  source_dest_check      = "false"
+  associate_public_ip_address = "true"
+  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+  user_data = data.template_file.user_data.rendered
+  key_name = "home desktop"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
 }
