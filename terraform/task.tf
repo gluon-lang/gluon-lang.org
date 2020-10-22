fc0b3db2a45ebdeae710a68535f0eb98a2a77092
Markus Westerlind <marwes91@gmail.com>
2020-10-23 01:38:37 +0200

    Working cloudwatch logs

--- terraform/task.tf
+++ terraform/task.tf
@@ -30,7 +30,74 @@ locals {
 DEFINITION
 }
 
+resource "aws_api_gateway_rest_api" "gluon-lang" {
+    name = "gluon-lang"
+}
 
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.gluon-lang.id
+   parent_id   = aws_api_gateway_rest_api.gluon-lang.root_resource_id
+   path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+   rest_api_id   = aws_api_gateway_rest_api.gluon-lang.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.gluon-lang.id
+   resource_id = aws_api_gateway_method.proxy.resource_id
+   http_method = aws_api_gateway_method.proxy.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.gluon-lang.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.gluon-lang.id
+   resource_id   = aws_api_gateway_rest_api.gluon-lang.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.gluon-lang.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.gluon-lang.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "gluon-lang" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.gluon-lang.id
+   stage_name  = "test"
+}
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.gluon-lang.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.gluon-lang.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.gluon-lang.invoke_url
+}
 
 resource "aws_lambda_function" "gluon-lang" {
     function_name = "gluon-lang"
@@ -50,7 +117,7 @@ resource "aws_lambda_function" "gluon-lang" {
 # IAM role which dictates what other AWS services the Lambda function
 # may access.
 resource "aws_iam_role" "lambda_exec" {
-   name = "serverless_example_lambda"
+   name = "gluon_lang_lambda"
 
    assume_role_policy = <<EOF
 {
@@ -68,4 +135,32 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
