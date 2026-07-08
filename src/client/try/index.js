920340b4f05a2909153240d55e8bd54505a07c8c
Markus Westerlind <marwes91@gmail.com>
2026-07-09 01:11:32 +0200

    perf: Lazy load elm

--- src/client/try/index.js
+++ src/client/try/index.js
@@ -3,14 +3,15 @@
 require('./index.html');
 require('./styles.scss');
 
-var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
+import('./Main.elm').then(Elm => {
+    var mountNode = document.getElementById('main');
 
-var app = Elm.Elm.Main.init({
-    node: mountNode,
-    flags: {
-        origin: document.location.origin,
-        pathname: document.location.pathname,
-        href: document.location.href,
-    },
+    var app = Elm.Elm.Main.init({
+        node: mountNode,
+        flags: {
+            origin: document.location.origin,
+            pathname: document.location.pathname,
+            href: document.location.href,
+        },
+    });
 });
