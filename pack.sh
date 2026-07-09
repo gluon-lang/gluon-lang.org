70dfd47c8948542df3f7250dde6444442caa2aac
Markus Westerlind <marwes91@gmail.com>
2026-07-09 20:01:30 +0200

    fix: Don't shadow the get router for static files

--- pack.sh
+++ pack.sh
@@ -11,8 +11,9 @@ zip --recurse-paths target/lambda.zip \
   bootstrap \
   Cargo.lock \
   public \
+  target/dist \
   src
 
 cp -r target/x86_64-unknown-linux-gnu/release/try_gluon target/
 cd target
-zip --recurse-paths lambda.zip dist doc try_gluon
+zip --recurse-paths lambda.zip doc try_gluon
